73b811cc63875622f912d334a4f96c265df7d34d
Rost911 <rostshulym@gmail.com>
2026-04-22 19:02:59 +0200

    Practice with Playwright Authorization

--- playwright_registration.py
+++ playwright_registration.py
@@ -24,3 +24,47 @@ with sync_playwright() as playwright:
 
     assert dashboard_text.text_content() == "Dashboard"
     page.wait_for_timeout(5000)
+
+
+from playwright.sync_api import sync_playwright
+
+# Open the browser using Playwright
+with sync_playwright() as playwright:
+    # Launch Chromium browser in headed mode (not headless)
+    browser = playwright.chromium.launch(headless=False)
+
+    # Create a new browser context (new session isolated from others)
+    context = browser.new_context()
+
+    # Open a new page within the context
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    # Save browser state (cookies and localStorage) to a file for future use
+    context.storage_state(path="browser-state.json")
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+
+    context = browser.new_context(
+        storage_state="browser-state.json"  # Specify the file with the saved state
+    )
+
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)
