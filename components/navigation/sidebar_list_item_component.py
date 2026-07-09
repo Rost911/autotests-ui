8c9cc03e3365a6319a6598d4cafba00cf12ca736
Rost911 <rostshulym@gmail.com>
2026-07-09 22:04:31 +0200

    Practicing Page Factory + fix review issues

--- components/navigation/sidebar_list_item_component.py
+++ components/navigation/sidebar_list_item_component.py
@@ -1,30 +1,29 @@
-from playwright.sync_api import Page, expect
+from typing import Pattern
+
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
-from typing import Pattern
+from elements.button import Button
 from elements.icon import Icon
 from elements.text import Text
-from elements.button import Button
 
 
 class SidebarListItemComponent(BaseComponent):
-
     def __init__(self, page: Page, identifier: str):
         super().__init__(page)
-        self.identifier = identifier
 
-        self.icon = Icon(page, '{identifier}-drawer-list-item-icon', ' Sidebar Icon')
-        self.title = Text(page, '{identifier}-drawer-list-item-title-text', ' Sidebar Title')
-        self.button = Button(page, '{identifier}-drawer-list-item-button', ' Sidebar Button')
+        self.icon = Icon(page, f'{identifier}-drawer-list-item-icon', 'Icon')
+        self.title = Text(page, f'{identifier}-drawer-list-item-title-text', 'Title')
+        self.button = Button(page, f'{identifier}-drawer-list-item-button', 'Button')
 
     def check_visible(self, title: str):
-        self.icon.check_visible(identifier=self.identifier)
+        self.icon.check_visible()
 
-        self.title.check_visible(identifier=self.identifier)
-        self.title.check_have_text(title, identifier=self.identifier)
+        self.title.check_visible()
+        self.title.check_have_text(title)
 
-        self.button.check_visible(identifier=self.identifier)
+        self.button.check_visible()
 
     def navigate(self, expected_url: Pattern[str]):
-        self.button.click(identifier=self.identifier)
+        self.button.click()
         self.check_current_url(expected_url)
