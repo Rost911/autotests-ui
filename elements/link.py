32b62b066e8b07130f139eaf1d3243551d7d4de0
Rost911 <rostshulym@gmail.com>
2026-07-16 21:15:31 +0200

    Adding Allure Steps to autotests

--- elements/link.py
+++ elements/link.py
@@ -3,5 +3,9 @@ from playwright.sync_api import Locator
 
 
 class Link(BaseElement):
+    @property
+    def type_of(self) -> str:
+        return "link"
+
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(**kwargs).nth(nth)
